17331f7cbe92793e0d31847e614f9d8f31a521ce
gaurav <gksingh1106@gmail.com>
2026-06-26 16:13:02 +0530

    feat: add UI button and link them to playRound logic

--- script.js
+++ script.js
@@ -16,36 +16,35 @@ function getHumanChoice(){
 }
 
 
-function playGame(){
-
     let humanScore = 0;
     let computerScore = 0;
 
-
-    // play 1 round and increment score depending on who win
-    function playRound(humanChoice , computerChoice){
-        const human = humanChoice.toLowerCase();
-        const computer = computerChoice.toLowerCase();
-        if (human === computer){
-            console.log("It's a tie! both chosed " + computerChoice + ".")
+// play 1 round and increment score depending on who win
+function playRound(humanChoice , computerChoice){
+    const human = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+    if (human === computer){
+        console.log("It's a tie! both chosed " + computerChoice + ".")
+    } else if ((human === "scissor" && computer === "paper") || (human === "rock" && computer === "scissor") || (human === "paper" && computer === "rock")){
+        console.log("You won! " + humanChoice + " beats " + computerChoice + ".");
+        humanScore++;
+    } else { console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++;
         }
-        else if ((human === "scissor" && computer === "paper") || (human === "rock" && computer === "scissor") || (human === "paper" && computer === "rock")){
-            console.log("You won! " + humanChoice + " beats " + computerChoice + ".");
-            humanScore++;
-        } else { console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
-            computerScore++;
-        }
-    }
-
-    // run loop 5 time and call playRound, getHumanChoice and getComputerChoice each time
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-        
-        console.log("Your score " + humanScore + ", Computer score " + computerScore);
-    }
 }
 
-playGame();
+const selectedRock = document.querySelector(".rock");
+const selectedPaper = document.querySelector(".paper");
+const selectedScissor = document.querySelector(".scissor");
+
+selectedRock.addEventListener("click", () => {
+    playRound("Rock", getComputerChoice());
+});
+
+selectedPaper.addEventListener("click", ()=> {
+    playRound("Paper", getComputerChoice());
+});
+
+selectedScissor.addEventListener("click", () => {
+    playRound("Scissor", getComputerChoice());
+});
