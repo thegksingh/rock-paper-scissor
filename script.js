a63986be6af4f2ece2462eafebf5a01cdf92fac7
gaurav <gksingh1106@gmail.com>
2026-06-26 17:09:06 +0530

    feat: impelement live score using innerText

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ function playRound(humanChoice , computerChoice){
     const human = humanChoice.toLowerCase();
     const computer = computerChoice.toLowerCase();
     if (human === computer){
-        gameResult.textContent = "It's a tie! both chosed " + computerChoice + ".";
+        gameResult.innerText = "It's a tie! both chosed " + computerChoice +  ".\nScore: Human - " + humanScore + " Computer - " + computerScore;
     } else if ((human === "scissor" && computer === "paper") || (human === "rock" && computer === "scissor") || (human === "paper" && computer === "rock")){
-        gameResult.textContent = "You won! " + humanChoice + " beats " + computerChoice + ".";
         humanScore++;
-    } else { gameResult.textContent = "You lose! " + computerChoice + " beats " + humanChoice + ".";
-        computerScore++;
+        gameResult.innerText = "You won this round! " + humanChoice + " beats " + computerChoice + ".\n Score: Human - " + humanScore + " Computer - " + computerScore;
+    } else { computerScore++;
+        gameResult.innerText = "You lose this round! " + computerChoice + " beats " + humanChoice + ".\nScore: Human - " + humanScore + " Computer - " + computerScore;
         }
 }
 
